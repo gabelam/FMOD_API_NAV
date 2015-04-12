92dbc43c8f96a18e229575255aa05474375c4cb2
Gabe Lamorie <gabelam@hotmail.com>
2015-04-12 12:34:31 -0700

    Initial Commit

---
+++ Users/gabriellamorie/Desktop/Programming/FMOD_API_NAV/FMOD_API_NAV/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FMOD_API_NAV
+//
+//  Created by Gabriel Lamorie on 4/12/15.
+//  Copyright (c) 2015 C Programming. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
